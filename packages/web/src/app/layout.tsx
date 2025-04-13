6ab7952dfb3a296427128b73630e43cc260d2f3d
VpgMackan <mackan.developer@pm.me>
2025-04-13 23:37:23 +0200

    add @iconify/react for icon support; update HomePage component and localization; refactor global styles

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -3,6 +3,14 @@ import { NextIntlClientProvider } from "next-intl";
 import { getLocale } from "next-intl/server";
 import "./globals.css";
 
+import { Montserrat } from "next/font/google";
+
+const inter = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["700"],
+});
+
 export const metadata: Metadata = {
   title: "Flyr.Chat",
   description: "The ai chatbot for your files",
@@ -15,8 +23,8 @@ export default async function RootLayout({
 }) {
   const locale = await getLocale();
   return (
-    <html lang={locale}>
-      <body>
+    <html lang={locale} className={`${inter.variable}`}>
+      <body className="p-16 font-montserrat bg-blue-50 text-gray-900">
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,6 +1,18 @@
 import { useTranslations } from "next-intl";
+import { Icon } from "@iconify/react";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
-  return <h1>{t("title")}</h1>;
+
+  return (
+    <div>
+      <div className="flex text-5xl ">
+        <h1 className="mr-8 font-bold">{t("welcome", { name: "Markus" })}</h1>
+        <Icon icon="twemoji:waving-hand" flip="horizontal" />
+      </div>
+      <div className="flex text-3xl mt-8">
+        <h1 className="mr-8 font-bold">{t("yourPockets")}</h1>
+      </div>
+    </div>
+  );
 }
