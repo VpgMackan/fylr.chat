5bd22da9edcba649cee0cfa7bffaf893bdafd2b1
VpgMackan <mackan.developer@pm.me>
2025-04-13 22:55:56 +0200

    add internationalization support with next-intl; update layout and page components

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <div></div>;
+import { useTranslations } from "next-intl";
+
+export default function HomePage() {
+  const t = useTranslations("HomePage");
+  return <h1>{t("title")}</h1>;
 }
