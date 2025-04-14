937d73604564797feb1d49d82622ac0e5d417ab4
VpgMackan <mackan.developer@pm.me>
2025-04-14 12:51:29 +0200

    add localization support for PocketIdPage; implement Source component for displaying sources with details

--- packages/web/src/app/pocket/[id]/page.tsx
+++ packages/web/src/app/pocket/[id]/page.tsx
@@ -6,6 +6,11 @@ import { useEffect, useState } from "react";
 
 import { useRouter } from "next/navigation";
 
+import Button from "@/components/Button";
+import Source from "@/components/Source";
+import PinnedPod from "@/components/Podcast";
+import Chat from "@/components/Chat";
+
 export default function PocketIdPage({
   params,
 }: {
@@ -29,7 +34,84 @@ export default function PocketIdPage({
         <Icon icon="weui:back-outlined" onClick={() => router.back()} />
         <p className="ml-8 font-bold">🧠 Lorem</p>
       </div>
-      <p>{id}</p>
+
+      <div>
+        <div className="flex justify-between items-center space-x-4 mt-8 mb-4">
+          <p className="font-semibold text-3xl">{t("yourSources")}</p>
+          <div className="flex">
+            <Button
+              text={common("viewAll")}
+              className="mr-2"
+              onClick={() => router.push("/pocket")}
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
+          <Source
+            title="🧠 Lorem"
+            summery="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            size="2.4 KB"
+            imported="2025/04/13"
+            id="e57b8ddd-c118-43cf-a595-067579b62b97"
+            pocketId={id || ""}
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex justify-between items-center space-x-4 mt-8 mb-4">
+          <p className="font-semibold text-3xl">{t("mostRecent")}</p>
+          <div className="flex">
+            <Button
+              text={common("viewAll")}
+              className="mr-2"
+              onClick={() => router.push("/pocket")}
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
+          <Source
+            title="🧠 Lorem"
+            summery="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            size="2.4 KB"
+            imported="2025/04/13"
+            id="e57b8ddd-c118-43cf-a595-067579b62b97"
+            pocketId={id || ""}
+          />
+          <PinnedPod title="Lorem ipsum" pocket="Lorem" />
+          <Chat
+            title="Lorem ipsum"
+            pocket="Lorem"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+          <Chat
+            title="Lorem ipsum"
+            pocket="Lorem"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between items-center space-x-4 mt-8 mb-4">
+          <p className="font-semibold text-3xl">{t("shortcuts")}</p>
+        </div>
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <Button
+            text={t("viewChats")}
+            onClick={() => router.push("/pocket")}
+          />
+          <Button
+            text={t("viewSummaries")}
+            onClick={() => router.push("/pocket")}
+          />
+          <Button
+            text={t("viewPodcats")}
+            onClick={() => router.push("/pocket")}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ packages/web/src/components/Source.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useTranslations } from "next-intl";
+
+import { useRouter } from "next/navigation";
+
+export default function Pocket({
+  title,
+  summery,
+  size,
+  imported,
+  id,
+  pocketId,
+}: {
+  title: string;
+  summery: string;
+  size: string;
+  imported: string;
+  id: string;
+  pocketId: string;
+}) {
+  const router = useRouter();
+  const common = useTranslations("common");
+
+  const handleClick = () => {
+    router.push(`/pocket/${pocketId}/source/${id}`);
+  };
+
+  return (
+    <div
+      className="border border-gray-600 rounded-lg p-4 hover:shadow-md transition-shadow duration-200 flex flex-col justify-between"
+      onClick={handleClick}
+    >
+      <div>
+        <p className="font-semibold">{title}</p>
+        <p className="text-sm text-gray-500 mb-2">{summery}</p>
+      </div>
+
+      <div>
+        <hr />
+        <p className="text-xs mt-2">{common("size", { size: size })}</p>
+        <p className="text-xs">{common("imported", { date: imported })}</p>
+      </div>
+    </div>
+  );
+}
