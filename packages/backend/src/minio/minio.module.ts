d7bfd275db8dc89a4896534b83b1440516f037ae
VpgMackan <mackan.developer@pm.me>
2025-04-25 23:03:31 +0200

    Removed default variable in minio module

--- packages/backend/src/minio/minio.module.ts
+++ packages/backend/src/minio/minio.module.ts
@@ -15,11 +15,11 @@ export class MinioModule {
           inject: [ConfigService],
           useFactory: (cs: ConfigService): Minio.Client => {
             return new Minio.Client({
-              endPoint: cs.getOrThrow('MINIO_ENDPOINT') || 'localhost',
-              port: cs.getOrThrow('MINIO_PORT') || 9000,
+              endPoint: cs.getOrThrow('MINIO_ENDPOINT'),
+              port: parseInt(cs.getOrThrow('MINIO_PORT'), 10),
               useSSL: cs.getOrThrow('MINIO_USE_SSL') === 'true',
-              accessKey: cs.getOrThrow('MINIO_ACCESS_KEY') || '',
-              secretKey: cs.getOrThrow('MINIO_SECRET_KEY') || '',
+              accessKey: cs.getOrThrow('MINIO_ACCESS_KEY'),
+              secretKey: cs.getOrThrow('MINIO_SECRET_KEY'),
             });
           },
         },
