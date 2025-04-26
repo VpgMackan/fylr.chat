5d380f7cb696d1af09651c24fe348616c43b9154
VpgMackan <mackan.developer@pm.me>
2025-04-26 12:34:11 +0200

    Organized imports in users

--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './users.entity';
 import { Repository } from 'typeorm';
+
+import { User } from './users.entity';
+
 import { CreateUserDto } from './create-user.dto';
 import { UpdateUserDto } from './update-user.dto';
 
@@ -12,15 +14,14 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async findOneById(id: string): Promise<User | undefined> {
+  async findOneById(id: string): Promise<User> {
     const user = await this.usersRepository.findOneBy({ id });
-    if (!user) {
-      throw new NotFoundException(`User with ID "${id}" not found`);
-    }
+    if (!user) throw new NotFoundException(`User with ID "${id}" not found`);
+
     return user;
   }
 
-  async findOneByEmail(email: string): Promise<User | undefined> {
+  async findOneByEmail(email: string): Promise<User> {
     const user = await this.usersRepository.findOneBy({ email });
     if (!user) {
       throw new NotFoundException(`User with email "${email}" not found`);
