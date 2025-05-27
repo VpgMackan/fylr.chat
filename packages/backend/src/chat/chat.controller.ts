5f50d45520281d044ab20abdca4e2e11b148f5e1
VpgMackan <mackan.developer@pm.me>
2025-05-28 00:45:14 +0200

    Mapped out routes

--- packages/backend/src/chat/chat.controller.ts
+++ packages/backend/src/chat/chat.controller.ts
@@ -1,23 +1,69 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ChatService } from './chat.service';
+
+@UseGuards(AuthGuard)
 @Controller('chat')
 export class ChatController {
-  /**
-   * Create convo
-   * Delete convo
-   * Read convo + msg
-   * Update convo (Pocket ID, Metadata, Title)
-   * 
-   * Create Message
-   * Send Message
-   *    Stream Response
-   * 
-   * Delete Message
-   * Update Message (Metadata, Content)
-   * 
-   * 
-   * Lots of stuff needed to handle sources
-   *    Genereate queries for source vector based on message and convo
-   *    More
-   */
+  constructor(private chatService: ChatService) {}
+  // # Conversations General
+  // Get all conversations
+  @Get('/conversations')
+  getConversations() {}
+
+  // Get all messags
+  @Get('/conversation/:id/messages')
+  getMessages() {}
+
+  // # Conversation
+  // Create Convo
+  @Post('/conversation')
+  createConversation() {}
+
+  // Delete Convo
+  @Delete('/conversation/:id')
+  deletConversation() {}
+
+  // Update Convo (Pocket ID, Metadata, Title)
+  @Patch('/conversation/:id')
+  updateConversation() {}
+
+  // Read Convo + msg
+  @Get('/conversation/:id')
+  getConversation() {}
+
+  // #Messages
+  // Create Message
+  @Post('/message')
+  createMessage() {
+    // Store message in database
+    // Ask llm for rag question based on chat and context
+    // Get the sources
+    // Send all of the information to a LLM
+    // Send resonse to user / Stream if possible
+  }
+
+  // Get Message
+  @Get('/message/:id')
+  getMessage() {}
+
+  // Regenerate
+  @Post('/message/:id/regenerate')
+  regenerateMessage() {}
+
+  // Delete Message
+  @Delete('/message/:id')
+  deleteMessage() {}
+
+  // Update Message (Metadata, Content)
+  @Patch('/message/:id')
+  updateMessage() {}
 }
