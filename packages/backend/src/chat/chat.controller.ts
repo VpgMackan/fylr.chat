af95678d10a52be5e26fcc3d46df5303ba58b243
VpgMackan <mackan.developer@pm.me>
2025-05-28 00:24:14 +0200

    Planned controller

--- packages/backend/src/chat/chat.controller.ts
+++ packages/backend/src/chat/chat.controller.ts
@@ -1,4 +1,23 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  /**
+   * Create convo
+   * Delete convo
+   * Read convo + msg
+   * Update convo (Pocket ID, Metadata, Title)
+   * 
+   * Create Message
+   * Send Message
+   *    Stream Response
+   * 
+   * Delete Message
+   * Update Message (Metadata, Content)
+   * 
+   * 
+   * Lots of stuff needed to handle sources
+   *    Genereate queries for source vector based on message and convo
+   *    More
+   */
+}
