b8976e97c7cf16fa05ffc554f51a77666a03bd9c
Markus Nielsen <52716811+VpgMackan@users.noreply.github.com>
2025-10-06 19:11:11 +0200

    Added tool file

---
+++ packages/backend/src/chat/tool.ts
@@ -0,0 +1,44 @@
+const tools = [
+  {
+    type: 'function',
+    function: {
+      name: 'search_documents',
+      description: 'Searches for relevant text chunks within the pocket.',
+      parameters: {
+        type: 'object',
+        properties: {
+          query: {
+            type: 'string',
+            description: 'The natural language query for the vector search.',
+          },
+          source_ids: {
+            type: 'array',
+            items: { type: 'string' },
+            description: 'Optional list of source IDs to restrict search.',
+          },
+        },
+        required: ['query'],
+      },
+    },
+  },
+  {
+    type: 'function',
+    function: {
+      name: 'list_sources_in_pocket',
+      description:
+        'Lists all available source documents in the current pocket.',
+      parameters: { type: 'object', properties: {} },
+    },
+  },
+  {
+    type: 'function',
+    function: {
+      name: 'provide_final_answer',
+      description:
+        'Call this function ONLY when you have gathered all necessary information and are ready to provide the final, complete answer to the user. This function will return a full answer based on your thoughts.',
+      parameters: { type: 'object', properties: {} },
+    },
+  },
+];
+
+export default tools;
